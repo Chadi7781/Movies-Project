094637ed8e3f2851b48543b449e2b16ac82192d9
Chadi7781 <troudishedy6@gmail.com>
2022-05-10 10:46:14 +0200

    cleanup react

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -9,22 +9,9 @@ import UseEffectExamples from './components/UseEffectExamples';
 export function App() {
   const [hide,setHide]= useState(false);
   return (
-    <div className="App">
-      {/* <Simple/>
-      <MapExample/> */}
-      <input type="checkbox" onChange={()=>setHide(!hide) }/>Hide
-      {
-        hide ? null :      <UseEffectExamples/>
- 
-      }
-      <ValueContext.Provider value={hide}>
-
-      <GrandParent/>
-
-      </ValueContext.Provider>
-
-      
-    </div>
+    <>
+    
+    </>
   );
 }
 
